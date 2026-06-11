454a0df71165c156b7074e345d64b925c707b546
박민식 <jaden2973@naver.com>
2026-06-11 17:28:10 +0900

    fix: 가제보 월드 경로를 팀장(Desktop)과 팀원 모두 호환되도록 동적 수정

--- ros2_ws/src/shelfa_gazebo/launch/shelfa_sim.launch.py
+++ ros2_ws/src/shelfa_gazebo/launch/shelfa_sim.launch.py
@@ -9,8 +9,11 @@ def generate_launch_description():
     pkg_gazebo_ros = get_package_share_directory('gazebo_ros')
     pkg_turtlebot3_gazebo = get_package_share_directory('turtlebot3_gazebo')
 
-    # Gazebo 모델 패스 환경변수 설정
-    shelfa_dir = os.path.expanduser('~/Shelfa')
+    # 팀장님 경로(~/Desktop/Shelfa)와 팀원 경로(~/Shelfa)를 모두 지원하도록 동적 설정
+    shelfa_dir = os.path.expanduser('~/Desktop/Shelfa')
+    if not os.path.exists(shelfa_dir):
+        shelfa_dir = os.path.expanduser('~/Shelfa')
+        
     gazebo_model_path = os.environ.get('GAZEBO_MODEL_PATH', '')
     if gazebo_model_path:
         gazebo_model_path += ':'
